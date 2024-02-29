293af13bd35e700939bf67518e77c39cdb856dbf
paulo <paulo.vsilva@localiza.com>
2024-02-29 14:44:22 -0400

    feat: vehicle mocks

---
+++ app/mocks/vehicle-mock.ts
@@ -0,0 +1,47 @@
+import img from "../assets/images/vehicle.jpeg";
+import { VehicleType } from "../types/vehicle";
+
+export const vehiclesMock: VehicleType[] = [
+  {
+    img: img,
+    name: "Mini Cooper 2020",
+    status: "Available",
+    reservation: "11/07",
+    stars: 5,
+  },
+  {
+    img: img,
+    name: "Mini Cooper 2020",
+    status: "Available",
+    reservation: "11/07",
+    stars: 5,
+  },
+  {
+    img: img,
+    name: "Mini Cooper 2020",
+    status: "Available",
+    reservation: "11/07",
+    stars: 5,
+  },
+  {
+    img: img,
+    name: "Mini Cooper 2020",
+    status: "Available",
+    reservation: "11/07",
+    stars: 5,
+  },
+  {
+    img: img,
+    name: "Mini Cooper 2020",
+    status: "Available",
+    reservation: "11/07",
+    stars: 5,
+  },
+  {
+    img: img,
+    name: "Mini Cooper 2020",
+    status: "Available",
+    reservation: "11/07",
+    stars: 5,
+  },
+];
