c52ab0fc38d49e00c392c2c8212d94d0190b93f1
paulo <paulo.vsilva@localiza.com>
2024-02-29 14:38:41 -0400

    feat: web and mobile version

---
+++ app/pages/Web.tsx
@@ -0,0 +1,9 @@
+import VehicleTable from "../components/VehicleTable";
+
+const Web = () => (
+  <div className="px-16 w-full overflow-auto hidden md:block">
+    <VehicleTable />
+  </div>
+);
+
+export default Web;
